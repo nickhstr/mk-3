5fafd404990475881e60643aaedab2f1c16fa8cf
nickhstr <nickhstr@gmail.com>
2019-01-23 13:53:59 -0500

    feat: add hello world template

--- routes/index.go
+++ routes/index.go
@@ -1,10 +1,19 @@
 package routes
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi"
 )
+
+var indexTemplate = template.Must(template.ParseFiles("templates/index.html"))
+
+type indexStruct struct {
+	Title string
+}
 
 // Index handles requests to the root route
 func Index(r *chi.Mux) {
@@ -12,7 +21,15 @@ func Index(r *chi.Mux) {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
+	wd, _ := os.Getwd()
+	fmt.Printf("os.Getwd: %v\n", wd)
+
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hello World!"))
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	data := indexStruct{
+		Title: "Hello World!",
+	}
+
+	indexTemplate.Execute(w, data)
 }
