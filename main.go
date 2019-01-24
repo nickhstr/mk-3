f1292e16cf2aec4cd386aa08dda427ea7c69fec2
nickhstr <nickhstr@gmail.com>
2019-01-24 16:13:17 -0500

    refactor: use 'cmd' convention

--- main.go
+++ main.go
@@ -1,21 +1,9 @@
 package main
 
 import (
-	"github.com/nickhstr/goweb/env"
-	_ "github.com/nickhstr/goweb/logger"
-	"github.com/nickhstr/goweb/middleware"
-	"github.com/nickhstr/goweb/server" // nolint: gotype
-	"github.com/nickhstr/mk-3/routes" // nolint: gotype
+	"github.com/nickhstr/mk-3/cmd" // nolint: gotype
 )
 
 func main() {
-	mux := middleware.Create(middleware.Config{
-		AppName:     env.AppName(),
-		AppVersion:  env.Get("APP_VERSION"),
-		GitRevision: env.Get("GIT_COMMIT"),
-		Region:      env.Get("REGION"),
-		Handler: routes.Router,
-	})
-
-	server.Start(mux)
+	cmd.Serve()
 }
