bc447d21884470e1a7d30c8176269e038da61d78
nickhstr <nickhstr@gmail.com>
2019-01-27 23:28:23 -0500

    feat: add some animejs

--- client/components/title-links/index.js
+++ client/components/title-links/index.js
@@ -1,4 +1,5 @@
 import { LitElement, html } from 'lit-element';
+import anime from 'animejs/lib/anime.es';
 import styles from './styles.css';
 
 export class TitleLinks extends LitElement {
@@ -17,10 +18,20 @@ export class TitleLinks extends LitElement {
     return 'title-links';
   }
 
+  firstUpdated() {
+    const linksNodes = this.shadowRoot.getElementById('title-links').children;
+    anime({
+      targets: linksNodes,
+      translateY: 20,
+      opacity: 1,
+      delay: anime.stagger(100),
+    });
+  }
+
   render() {
     return html`
       <style>${styles.toString()}</style>
-      <ul class="links">
+      <ul class="links" id="title-links">
         ${this.links.map(link => html`
           <li class="link">
             <a href="${link.href}">${link.name}</a>
