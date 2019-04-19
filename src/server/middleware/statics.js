a9cc5f2490730d41ea569b5e023f4c387568e111
nickhstr <nickhstr@gmail.com>
2019-04-19 00:18:50 -0400

    refactor: update maxages

--- src/server/middleware/statics.js
+++ src/server/middleware/statics.js
@@ -3,11 +3,16 @@ import send from 'koa-send';
 import mount from 'koa-mount';
 import serve from 'koa-static';
 
+const MAXAGE = {
+  WEEK: 604800000, // 1000 * 60 seconds * 60 minutes * 24 hours * 7 days = 1 week = 604800 seconds
+  MONTH: 2628001152, // WEEK * 4.34524
+};
+
 export function staticFavicon() {
   return async (ctx, next) => {
     if (ctx.path === '/favicon.ico') {
       await send(ctx, 'dist/web/favicon.ico', {
-        maxage: 604800, // 60 seconds * 60 minutes * 24 hours * 7 days = 1 week = 604800 seconds
+        maxage: MAXAGE.MONTH,
       });
 
       return;
@@ -18,7 +23,7 @@ export function staticFavicon() {
 
 export function staticAssets() {
   return mount('/assets', serve('dist/web', {
-    maxage: 0, // TODO: change this to a reasonable value when development has calmed down a bit
+    maxage: MAXAGE.WEEK,
   }));
 }
 
